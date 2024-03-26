0a042e6b2d8998f85e0f07dc7fe8f7807e9713ea
Robert Schofield <rschofield1992@gmail.com>
2024-03-26 15:32:28 -0400

    inital

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //create two integar variables
+
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remander = a % b;
+            var sum = a + b;
+            var sum1 = a - b;
+
+
+            Console.WriteLine($"{a} + {b} is {sum}");
+            Console.WriteLine($"{a} - {b} is {sum1}");
+            Console.WriteLine($"{a} is / {b} is {quotient} remander {remander}");
+            Console.WriteLine($"{a} % {b} is {remander}");
+            Console.WriteLine($"{a} / {b} is {quotient}");
+
+            Console.WriteLine("enter radius");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+            
+
+
+            Console.WriteLine(AreaOfCircle(radius));
+
         }
+
+        //method to calculate radius of circle
+
+
+        public static Double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+
+            
+
+
+
+
+
+
+        }
+
+            
+            
+
     }
 }
